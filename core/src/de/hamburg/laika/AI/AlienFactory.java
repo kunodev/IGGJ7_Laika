704f751efeb18df5db203e3643f69318481d2f6c
dendendev <dat.luriad@gmail.com>
2014-11-15 00:08:20 +0100

    test

---
+++ core/src/de/hamburg/laika/AI/AlienFactory.java
@@ -0,0 +1,22 @@
+package de.hamburg.laika.AI;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+
+import de.kuro.lazyjam.cdiutils.annotations.Update;
+
+public class AlienFactory {
+	
+	private ArrayList<String> alienType = new ArrayList<String>();
+	
+	@Update
+	public void update(Input i, Vector2 pos) {
+		
+	}
+	
+	public void registerAlienType(String type) {
+		
+	}
+}
--- core/src/de/hamburg/laika/Laika.java
+++ core/src/de/hamburg/laika/Laika.java
@@ -1,12 +1,8 @@
 package de.hamburg.laika;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import de.hamburg.laika.AI.AlienFactory;
 import de.hamburg.laika.player.PlayerOne;
 import de.kuro.lazyjam.ecmodel.concrete.GameObject;
 import de.kuro.lazyjam.ecmodel.concrete.GameState;
@@ -25,8 +21,12 @@ public class Laika extends LazyJamApplicationAdapter {
 		super.create();
 		GameState gs = new GameState();
 		this.gscm.initMainGameState(gs);
+		
 		GameObject laika = new GameObject(new Vector2(),gs);
 		laika.addComponent(new PlayerOne());
+		
+		AlienFactory alienFac = new AlienFactory();		
+		laika.addComponent(alienFac);
 		
 	}
 }
