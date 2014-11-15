23165891f5eba93774f9ebfa417b2ed0ff6554bc
Marco Krüger <krgsch@gmail.com>
2014-11-15 10:22:42 +0100

    ship: avoid self damage

--- core/src/de/kuro/lazyjam/ecmodel/concrete/components/ExtraSimpleCollisionComponent.java
+++ core/src/de/kuro/lazyjam/ecmodel/concrete/components/ExtraSimpleCollisionComponent.java
@@ -3,24 +3,26 @@ package de.kuro.lazyjam.ecmodel.concrete.components;
 import com.badlogic.gdx.math.Vector2;
 
 import de.hamburg.laika.EnemyType.HealthComponent;
+import de.hamburg.laika.player.PlayerControl;
 import de.kuro.lazyjam.cdiutils.annotations.Update;
 import de.kuro.lazyjam.ecmodel.concrete.GameObject;
 import de.kuro.lazyjam.ecmodel.concrete.GameState;
 
 public abstract class ExtraSimpleCollisionComponent {
-	
+
 	public int range = 10;
-	
+
 	@Update
 	public void collideWith(Vector2 pos, GameState gs, GameObject go) {
 		gs.gameObjects.stream()
-			.filter(e -> e.getComponent(HealthComponent.class) != null)
-			.filter(e -> e.getPos().dst(pos) <= range)
-			.filter(e -> e != go)
-			.forEach(e -> go.getComponent(ExtraSimpleCollisionComponent.class).collide(go, e, gs));
+				.filter(e -> e.getComponent(HealthComponent.class) != null)
+				.filter(e -> e.getPos().dst(pos) <= range)
+				.filter(e -> e.getComponent(PlayerControl.class) == null)
+				.filter(e -> e != go)
+				.forEach(e -> go.getComponent(ExtraSimpleCollisionComponent.class).collide(go, e, gs));
 	}
-	
+
 	public abstract void collide(GameObject thisGo, GameObject otherGo, GameState gs);
-	
-	
+
+
 }
