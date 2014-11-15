206a3c783496aa3be8f3902f8be1ce0ecb80ea5f
dendendev <dat.luriad@gmail.com>
2014-11-15 04:18:03 +0100

    IEnemyMovement, AlienFac

--- core/src/de/hamburg/laika/EnemyMovement/PursueTarget.java
+++ core/src/de/hamburg/laika/EnemyMovement/PursueTarget.java
@@ -2,10 +2,11 @@ package de.hamburg.laika.EnemyMovement;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+
 import de.kuro.lazyjam.cdiutils.annotations.Update;
 import de.kuro.lazyjam.ecmodel.concrete.GameObject;
 
-public class PursueTarget {
+public class PursueTarget implements IEnemyMovement {
 	private Vector2 toShip = new Vector2();
 
 	private final GameObject target;
