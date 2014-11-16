bbf5d32ac61d2a39023cf024ee2c51f1a1523eae
kuro <kuro@kuro-LaptopM2.(none)>
2014-11-16 01:14:28 +0100

    added pew PEW PEW!!!! SOUNDS

--- core/src/de/hamburg/laika/player/Bullet.java
+++ core/src/de/hamburg/laika/player/Bullet.java
@@ -1,14 +1,28 @@
 package de.hamburg.laika.player;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
+
 import de.hamburg.laika.EnemyType.HealthComponent;
 import de.kuro.lazyjam.cdiutils.annotations.Collide;
+import de.kuro.lazyjam.cdiutils.annotations.Update;
 import de.kuro.lazyjam.ecmodel.concrete.GameState;
 import de.kuro.lazyjam.ecmodel.concrete.tools.Collision;
 
 public class Bullet {
 
 	public int damage = 34;
+	boolean sounded = false; 
 
+	@Update
+	public void doShizzle(AssetManager assetMan) {
+		if(!sounded) {
+			sounded = true;
+			Sound s = assetMan.get("Laser_Shoot11.wav");
+			s.play();
+		}
+	}
+	
 	@Collide
 	public void collide(Collision co, GameState gs) {
 		co.thisGo.selfDestruct(gs);
