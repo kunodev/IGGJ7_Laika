2d7593b08ef8c3bda5c2bb25de275e0d01ef92d2
Marco Krüger <krgsch@gmail.com>
2014-11-16 05:20:10 +0100

    added health/collision to bag cat

---
+++ core/src/de/hamburg/laika/EnemyType/BagCat.java
@@ -0,0 +1,20 @@
+package de.hamburg.laika.EnemyType;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
+import de.hamburg.laika.player.Bullet;
+import de.kuro.lazyjam.cdiutils.annotations.Collide;
+import de.kuro.lazyjam.ecmodel.concrete.GameState;
+import de.kuro.lazyjam.ecmodel.concrete.tools.Collision;
+
+public class BagCat implements IEnemyType {
+	@Collide
+	public void kill(Collision co, GameState gs, HealthComponent hc, AssetManager assetMan) {
+		Sound s = assetMan.get("Hit_Hurt11.wav");
+		s.play();
+		co.otherGo.selfDestruct(gs);
+		if(hc.damage(co.otherGo.getComponent(Bullet.class).damage)) {
+			co.thisGo.selfDestruct(gs);
+		}
+	}
+}
